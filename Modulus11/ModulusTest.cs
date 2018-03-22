a24084507ac2be3cb05466d244d6d1b29c41c7ac
Kjetil Kværne <skarnes20@hotmail.com>
2018-03-22 12:44:53 +0100

    Initial version

---
+++ Modulus11/ModulusTest.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace Kjetil.Kata.Modulus11
+{
+    public class ModulusTest
+    {
+        [Theory]
+        [InlineData("1234.56.78903", true)]
+        [InlineData("1234.56.78902", false)]
+        public void Valid_AccountNumber_Expected(string accountNumber, bool expected)
+        {
+            Assert.Equal(expected, Modulus.IsMod11Valid(accountNumber));
+        }
+    }
+}
