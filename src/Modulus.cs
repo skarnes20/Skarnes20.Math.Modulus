733c54c9855520cdf728402cf3ca44b8dc8f99cf
Kjetil Kværne <kjetil.kvaerne@soprasteria.com>
2024-01-25 13:33:07 +0100

    Updated to .net 8

---
+++ src/Modulus.cs
@@ -0,0 +1,36 @@
+﻿namespace Kjetil.Math.Modulus;
+
+public class Modulus
+{
+    public static bool IsMod11Valid(string accountNumber)
+    {
+        var numbers = accountNumber
+            .Replace(".", string.Empty)
+            .ToCharArray()
+            .Select(x => int.Parse(x.ToString()))
+            .ToList();
+
+        return 11 - Sum(numbers) % 11 == CheckDigit(numbers);
+    }
+
+    private static int Sum(IReadOnlyList<int> numbers)
+    {
+        var totals = 0;
+        var factor = 2;
+        for (var i = numbers.Count-2; i >= 0; i--)
+        {
+            totals += numbers[i] * factor++;
+            if (factor > 7)
+            {
+                factor = 2;
+            }
+        }
+
+        return totals;
+    }
+
+    private static int CheckDigit(IReadOnlyList<int> numbers)
+    {
+        return numbers[^1];
+    }
+}
